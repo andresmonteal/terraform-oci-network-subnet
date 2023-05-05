8b5285c436632b77da1a848f62bc4714040ccadf
Andres Montealegre <andres.montealegre@oracle.com>
2023-05-04 20:25:03 -0500

    fix: removes tags from vcn for subnet

--- subnet.tf
+++ subnet.tf
@@ -3,7 +3,6 @@
 
 locals {
   dhcp_default_options = data.oci_core_dhcp_options.dhcp_options.options.0.id
-  defined_tags         = data.oci_core_vcns.vcns.virtual_networks[0].defined_tags
   vcn_id               = data.oci_core_vcns.vcns.virtual_networks[0].id
   security_list_ids    = [data.oci_core_security_lists.sec_lists.security_lists[0].id]
   default_freeform_tags = {
@@ -21,7 +20,7 @@ resource "oci_core_subnet" "vcn_subnet" {
   vcn_id         = local.vcn_id
 
 
-  defined_tags               = var.defined_tags == null ? local.defined_tags : var.defined_tags
+  defined_tags               = var.defined_tags
   dhcp_options_id            = local.dhcp_default_options
   display_name               = var.display_name
   dns_label                  = var.dns_label
@@ -39,7 +38,7 @@ module "route_table" {
   compartment_id = var.compartment_id
   subnet_ids     = [oci_core_subnet.vcn_subnet.id]
   vcn_id         = local.vcn_id
-  defined_tags   = var.defined_tags == null ? local.defined_tags : var.defined_tags
+  defined_tags   = var.defined_tags
   freeform_tags  = local.merged_freeform_tags
 
   rules = can(each.value["rules"]) ? each.value["rules"] : {}
